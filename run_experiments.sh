3d470ddbcdb24b617e4fc95e1695cc633e8693d5
stefanoviel <stefi.viel@gmail.com>
2024-11-13 12:03:31 +0100

    run experiments with clipping

--- run_experiments.sh
+++ run_experiments.sh
@@ -9,18 +9,22 @@ q_pairs=(1 2 3 5)
 # Array of seeds
 seeds=(42 123 456 789 1024)
 
+# Array of q_std_clip
+q_std_clip=(0.1 1 10 100)
+
 # Add delay between process launches
 delay_seconds=2
 
 for q in "${q_pairs[@]}"; do
     for seed in "${seeds[@]}"; do
-        echo "Starting process with q_pairs=$q and seed=$seed"
-        
-        # Run process and wait for the specified delay
-        (python -m firl.irl_samples configs/samples/agents/hopper.yml $q $seed > outputs/run_q${q}_seed${seed}.log 2>&1) &
-        
-        # Add delay between launches
-        sleep $delay_seconds
+        for q_std_clip in "${q_std_clip[@]}"; do
+            echo "Starting process with q_pairs=$q and seed=$seed"
+            
+            # Run process and wait for the specified delay
+            (python -m firl.irl_samples --config configs/samples/agents/hopper.yml --num_q_pairs $q --seed $seed --q_std_clip $q_std_clip> outputs/run_q${q}_seed${seed}.log 2>&1) &
+            
+            # Add delay between launches
+            sleep $delay_seconds
     done
 done
 
